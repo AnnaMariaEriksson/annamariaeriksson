954844b28ed47485c4b3dc275418100df4179f37
Anna-Maria Eriksson <greatdanemaniac@gmail.com>
2024-01-20 19:25:23 +0100

    Remade the project with vue cli

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Jag är en junior webbutvecklare som letar efter första jobbet!</h1>
+    <p>
+      Egentligen är jag utbildad till systemutvecklare i .NET, men jag har helt
+      fastnat för webben!
+    </p>
+    <p>Sidan uppdateras allt eftersom.</p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1 class="header">Anna-Maria Eriksson</h1>
+    <h2 class="tagline">Otroligt driven (webb)utvecklare!</h2>
+    <div class="hero">
+      <p>
+        Jag heter Anna-Maria Eriksson och jag vill jobba med webben om jag får
+        välja!
+      </p>
+      <p>
+        Jag är extremt driven, nyfiken, hjälpsam, frågvis och helt underbar att
+        jobba med. Det säger i alla fall de företag som gett mig en möjlighet
+        att visa vad jag går för.
+      </p>
+      <p>
+        Sidan här uppdateras allt eftersom. Jag har en del projekt, och vill du
+        ha länk till dem, kontakta mig på
+        <a href="https://linkedin.com/annamariaeriksson"> LinkedIn</a>.
+      </p>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+<script setup lang="ts"></script>
 
-export default defineComponent({
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-});
-</script>
+<style>
+.home {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+.header,
+.tagline {
+  grid-column: 1;
+}
+</style>
